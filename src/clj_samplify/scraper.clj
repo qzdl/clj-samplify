01a3b9a51a81d6ee56000c3ef5d7d676939db9bb
qzdl <accounts@samuelculpepper.com>
2020-01-26 03:02:36 +0000

    Extend {scraper} search to grab links and parse track line-items
    - still playing around with enlive, finding ways to express my intent easier.
    - I would like to explore some concision over `track->info'; an extraction
    function for track-items, I feel like I should be be able to go straight into
    a map, or at the very least just clean up some duplication.

--- src/clj_samplify/scraper.clj
+++ src/clj_samplify/scraper.clj
@@ -8,23 +8,92 @@
 
 (defn node->text
   [n]
-  (-> first html/text))
+  (html/text (first n)))
 
-;; SEARCH
+;;; SEARCH
+;;
+
+
+(def ^:dynamic *search-url*
+  "https://whosampled.com/search/")
+
+(def ^:dynamic *search-page*
+  (->> ["halftime" "nas"]
+       (build-search *search-url*)
+       fetch-url))
+
+(def ^:dynamic *tophit-selector*
+  [html/root :.topHit])
+
+(def *track-fragment*
+  "Header/Body content for each section is built from a sequence of divs,
+  with each distinct section marked by the occurance of `:div.sectionHeader'.
+  It's not clear if we can rely on the above logic of 'all siblings until parent
+  closes' as `:.layout-container' holds other items.
+
+  Other things to note:
+  - after `:topHit', tracks are contained within `:ul.list', and share the
+  structure from detail pages"
+  "TODO")
+
 (defn build-search
   "Constructs query string params for url & list/vec of terms.
-Returns url as string.
+  Returns url as string.
 
   E.g.
-  (build-search *base-url* [\"halftime\" \"nas\"])
-  ;; => \"https://whosampled.com/?q=+halftime+nas\""
+  (build-search *search-url* [\"halftime\" \"nas\"])
+  ;; => \"https://whosampled.com/search/?q=+halftime+nas\""
   [url terms]
   (str url "?q="
        (apply str
               (mapcat #(str "+" %) terms))))
 
+(defn search->tophit [p]
+  (html/select p *tophit-selector*))
+
+(defn anchors->hrefs [a]
+  (map #(first (html/attr-values % :href)) a))
+
+(defn track->info [node]
+  ""
+  (let [title  (first (html/select node [:.title :.trackTitle]))
+        artist (first (html/select node [:.title :.trackArtist :a]))
+        text-result (map html/text [title artist])
+        link-result (anchors->hrefs [title artist])]
+    (zipmap [:title :artist :track-link :artist-link]
+            (concat text-result link-result))))
+
+(comment ".: Threading to the max
+  Here we're composing a bunch of steps to get to a valid search link,
+first from the bound `*search-page*', then a fresh e2e, dropping the
+init values straight into build-search. It's satisfying to put things together
+like this."
+
+         ;; iterate on bound values
+         (->> (search->tophit *search-page*)
+              track->info
+              :track-link
+              (str *base-url*)) ; "https://whosampled.com/Nas/Halftime"
+
+         ;; regress e2e
+         (->> ["halftime" "nas"]
+              (build-search *search-url*)
+              fetch-url
+              search->tophit
+              track->info
+              :track-link
+              (str *base-url*))
+
+         "I'm still thinking of how to write tests as a part of this process;
+still lots of reading to do."
+         ":threading:composition:")
+
+
 
 ;;; DEV-RESOURCES
+;;
+
+
 (comment "General: Dirty REPL
   When messing around with Clojure, it's common for me to try a few
 ranges of parameters to some function, I'm interested in, or even start writing
@@ -36,12 +105,13 @@ file APIs that haven't been closed or GC'ed. Stack Overflow has delivered an
 absolute gem to clear up the situation we describe here:
 ;; => "
 
-  )
-(ns-interns *ns*)
-(ns-unmap *ns* 'build-search_single-input)
-(ns-unmap *ns* 'build-search_n-inputs)
+         (ns-interns *ns*)
+         (ns-unmap *ns* 'build-search_single-input)
+         (ns-unmap *ns* 'build-search_n-inputs)
 
-(def *base-page*
+         ":repl:general:housekeeping:")
+
+(def ^:dynamic *base-page*
   "HTML from base-url"
   (fetch-url *base-url*))
 
@@ -50,24 +120,7 @@ absolute gem to clear up the situation we describe here:
   ([] (fetch-title (fetch-url *base-url*)))
   ([dirt] (html/select dirt [:head :title])))
 
-(defn dirt->test-data!
-  "Takes paydirt, evals all (dirt is probably lazy), dumps output to
-'{project root}/test/data/whosampled_`~name.edn'
-
-e.g. (-> (fetch-url *base-url*)
-         (dirt->test-data! % \"base\"))"
-  [dirt name]
-  (let [path (str "test/data/whosampled_" name ".edn")]
-    (-> (into () *base-page*)
-        (spit path))))
-
-
-(-> *base-page*
-    (html/select [:head :title])
-    first html/text)
-
-(comment
-  "Enlive: Initial Text Retrieval:
+(comment "Enlive: Initial Text Retrieval:
   My very first attempt to parse html with enlive. It seems this way of ripping
 out basic text nodes is clumsy, though it does seem to work.
 
@@ -87,24 +140,24 @@ This section will read linearly, in the present tense, as I think, research, and
 experiment."
 
   ;; initial reasoning
-  (first
-   (:content
-    (first
-     (explore-fetch *base-page*))))
+         (first
+          (:content
+           (first
+            (explore-fetch *base-page*))))
 
   ;; restructuring chained calls for stoile (later found out that no parens req'd)
-  (-> (explore-fetch *base-page*)
-      (first)
-      (:content)
-      (first))
+         (-> (explore-fetch *base-page*)
+             (first)
+             (:content)
+             (first))
 
   ;; confirmation of nil behaviour
-  (-> nil
-      (first)
-      (:content)
-      (first))
+         (-> nil
+             (first)
+             (:content)
+             (first))
 
-  "Picking this up later, it seems as though this approach is more general for
+         "Picking this up later, it seems as though this approach is more general for
 node->0th-content->0th-node. This of course can be 'surrounded' by other nodes;
 0-n (*) potential siblings/children depending on whether element is self-closing.
 For reference: https://github.com/cgrand/enlive/blob/119ab97dd683681683354b60e356ed748bad5e78/src/net/cgrand/enlive_html.clj#L132
@@ -113,12 +166,12 @@ For reference: https://github.com/cgrand/enlive/blob/119ab97dd683681683354b60e35
   It strikes me that I don't know what '0th-content' means in the context of node->0th-content->0th-node.
 When hitting:"
 
-  (-> *base-page*
-      (html/select)
-      [:head :title]) ; , d v to inspect
+         (-> *base-page*
+             (html/select)
+             [:head :title]) ; , d v to inspect
 
 
-  ".. it's a list of nodes that is returned. Maybe worth digging into html/select
+         ".. it's a list of nodes that is returned. Maybe worth digging into html/select
 to check out whats happening here; is every result from html/select a list? It
 would seem to follow the emphasis on set operations that;
  'if no matches for the given predicate(s) are found, return an empty list
@@ -128,14 +181,50 @@ would seem to follow the emphasis on set operations that;
 in the function only ever returning matches to specified predicates, or an empty
 list. The following is far from covering, but it helps to illustrate the point:"
 
-  (html/select nil [:head]) ; => ()
-  (html/select nil nil)     ; => ()
+         (html/select nil [:head]) ; => ()
+         (html/select nil nil)     ; => ()
 
 
-  "Harking back to the original node->0th-content->first piece, I'm not sure that
+         "Harking back to the original node->0th-content->first piece, I'm not sure that
 I have a need for such an extractor just yet, but it's good to explore
 datastructures in this fashion to best think about how to manipulate them. This
- (enlive) might be a good fit for a v0 mapping from ox:html-export->clojure data.")
+ (enlive) might be a good fit for a v0 mapping from ox:html-export->clojure data."
+
+         ":thoughtstream:live:")
+
+(defn dirt->test-data!
+  "With use for test data persistence.
+  Takes paydirt, evals all (dirt is probably lazy), dumps output to
+  '{project root}/test/data/whosampled_`~name.edn'
+
+e.g. (-> (fetch-url *base-url*)
+         (dirt->test-data! % \"base\"))"
+  [dirt name]
+  (let [path (str "test/data/whosampled_" name ".edn")]
+    (-> (into () *base-page*)
+        (spit path))))
+
+(comment "General: Style & Threading Macros
+  The following is an example of function application to get something useful.
+Side note: My go-to at this point is `->`, the top-down threading macro; it
+seems like a great way to structure chained function calls, but my concern is
+that each function is doing too much if a default is to reach for this macro.
+ "
+
+         (-> *base-page*
+             (html/select [:head :title])
+             node->text)
+
+         "Having asked the community on the usage of `->', I received some guidance
+around sense; it's important to understand what the threading macros are doing,
+so reading reference material on the subject:
+;; => https://clojure.org/guides/threading_macros
+;; => https://stuartsierra.com/2018/07/06/threading-with-style.
+  Ultimately though, my main take-away is that I'm focusing too much on problems
+of the implementation, of style, when what really matters at this stage of my
+learning is making some stuff work."
+
+         ":fundamentals:threading:")
 
 (comment "General: Destructuring and ISeq
   I'm trying to figure out how and why the datastructures:ISeq are transformed
@@ -146,4 +235,4 @@ datastructures in this fashion to best think about how to manipulate them. This
              first   ; => {[a b] [c]}
              first   ; => [[a b] [c]]
              first)  ; => [a b]
-         )
+         ":destructuring:sequences:")
