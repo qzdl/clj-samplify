392a71868dcbeb3f05b68cb6fc3ea63f646646bc
qzdl <accounts@samuelculpepper.com>
2020-01-27 08:05:34 +0000

    Add key-shake for declarative :key mapping, \nComment blocks on concision

--- src/clj_samplify/scraper.clj
+++ src/clj_samplify/scraper.clj
@@ -6,9 +6,6 @@
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn node->text
-  [n]
-  (html/text (first n)))
 
 ;;; SEARCH
 ;;
@@ -54,14 +51,176 @@
 (defn anchors->hrefs [a]
   (map #(first (html/attr-values % :href)) a))
 
-(defn track->info [node]
+;; a generalisation of this exists as `fn-node'
+(defn first-node
+  ([node]
+   (fn [pred] (first-node node pred)))
+  ([node pred]
+   (first (html/select node pred))))
+
+(-> (nsmap
+ {:text (shake-fn #(map html/text %))
+  :link (shake-fn anchors->hrefs)})
+    :link/title)
+
+(defn nsmap
+  "Namespace all unqualified keys in `m` with `n'.
+  Single arity takes a map of maps, returning inner maps with keys ns'd by the key
+  of "
+  ([m]
+   (map #(nsmap (% m) (name %)) (keys m)))
+  ([m n]
+   (reduce-kv
+    (fn [acc k v]
+      (let [new-kw (if (and (keyword? k)
+                            (not (qualified-keyword? k)))
+                     (keyword (str n) (name k))
+                     k)]
+        (assoc acc new-kw v)))
+    {} m)))
+
+(defn shake
+  [p-key]
+
+  (->> (p-key app-map)
+       f-node list f first
+       (assoc {} p-key)))
+
+(defn key-shake
+  "Interrogate map(s) for `f' of `pred' given `:key'.
+  Returns a map fitting the key structure of app-map, where the value
+  is the result of `f' applied to the return of `(html/select node pred)'.
+
+  If not `seq? nodes',
+  - given a seq of maps
+
+  shake:
+  - Given a key and a map,
+    look-up value (predicate) as p,
+    pass p to f-node as v,
+    apply f on v, assoc into map as key
+
+  [nodes]: return a closure on nodes over recur[n f a]
+  [nodes f app-map]:
+  TODO: apply f as seq of fns"
+  ([nodes]
+   (fn [f app-map] (key-shake nodes f app-map)))
+  ([nodes f app-map]
+   (let [app-keys (keys app-map)
+         f-node (first-node nodes)
+         shake (fn [p-key]
+                 (->> (p-key app-map)
+                      f-node list f first
+                      (assoc {} p-key)))]
+     (reduce merge (map shake app-keys)))))
+
+(comment "Closures: In search of concision
+An interesting descent into variadic madness.
+
+The below block was my first attempt at this, and I was *not* satisfied with the
+approach. For starters, the duplication is horrendous, both of repeated logic;
+`(first (html/select) ...)', and of symbols -- for each new binding, and/or
+value type (text, link) the expansion is shocking:"
+
+         ;; innocent
+         (let [node *search-page*
+               title  (first (html/select node [:.title :.trackTitle]))
+               artist (first (html/select node [:.title :.trackArtist :a]))
+               text-result (map html/text [title artist])
+               link-result (anchors->hrefs [title artist])]
+           (zipmap [:title :artist :track-link :artist-link]
+                   (concat text-result link-result)))
+
+         ;; add new type and field-value binding
+         (let [node *search-page*
+               title  (first (html/select node [:.title :.trackTitle]))
+               artist (first (html/select node [:.title :.trackArtist :a]))
+               year   (first (html/select node [:.title :.trackYear]))
+               text-result (map html/text [title artist year])
+               link-result (anchors->hrefs [title artist])]
+           baztype-result (map bazify [title artist year])
+           (zipmap [:title :artist :year
+                    :track-link :artist-link
+                    :title-baztype :artist-baztype :year-baztype]
+                   (concat text-result link-result)))
+
+         ".. this is simply not on. The first go at improving this lead me to
+write a pretty wacky function `key-shake' that allows a pretty nice declarative
+way of specifying what you'd like back. My understanding of the function I had
+just written was fairly poor, so the v0 caller looked like this:"
+
+         (let [shaker (key-shake *search-page*)
+               [title artist] [[:.title :.trackTitle] [:.title :.trackArtist :a]]
+               decl {:title title :artist artist}
+               links  (shaker anchors->hrefs decl)
+               text   (shaker #(map html/text %) decl)]
+           (reduce merge links text))
+
+         "If looking at that gave you a strange feeling, it's because there's a
+collison in the key names; the horrible repitition has been cleared up, but the
+dynamic naming has not been solved -- the semantics of `merge' are like an upsert,
+so the last value to be bound will be of the coll `text' in this case.
+  I landed on namespacing my keys to ensure uniqueness between value-types
+leaning on [[this stack overflow answer][https://stackoverflow.com/questions/43722091/clojure-programmatically-namespace-map-keys]] for the initial framing of the functionality.
+"
+
+         ;; current best
+         (let [shaker (key-shake *search-page*)
+               sq {:title [:.title :.trackTitle]
+                   :artist [:.title :.trackArtist :a]}]
+           (nsmap {:links (shaker anchors->hrefs sq)
+                   :text  (shaker #(map html/text %) sq)}))
+
+         ;; ideal?
+         (nsmap
+          (shaker *search-page*
+                  {:links anchors->hrefs
+                   :text  #(map html/text %)}
+                  {:title [:.title :.trackTitle]
+                   :artist [:.title :.trackArtist :a]}))
+
+         ".. I think this is a step in the right direction, although there's
+still a few sketchy things going on behind the scenes, such as the use of
+`first-node' in key-shake, and closures on `nodes' pervading "
+
+         (defn shake-fn [f]
+           (key-shake *search-page* f
+                      {:title [:.title :.trackTitle]
+                       :artist [:.title :.trackArtist :a]}))
+
+         (nsmap
+          {:text (shake-fn #(map html/text %))
+           :link (shake-fn anchors->hrefs)})
+
+         (nsmap {:title {"aa" "bb"}})
+
+         "Reducing the amount of needless repition in a program is great, but
+there are still problems with the approach as seen above; the first map is the
+value-type {:name fn}, really just a function to apply to whatever is returned
+by the predicates passed to `first-node', with the key as ready to namespace.
+The second map specifies some binding name, and the predicate to filter the
+html-resource. Every (k0,v0)function in the first map will be applied to the
+values returned from (k1,v1)preds in the second map, bound as
+{:k0/k1 (v0 (v1 html-resource)}. ")
+
+(comment "General: Sane things about interning & symbols
+- A key is still a function of a map, even if bound to a sym. Bliss."
+
+         (let [key-as-sym :my-key]
+           (key-as-sym {:my-key "okay"}))
+
+         (assoc)
+
+         ":essence:nice:")
+
+(defn track->info
   ""
-  (let [title  (first (html/select node [:.title :.trackTitle]))
-        artist (first (html/select node [:.title :.trackArtist :a]))
-        text-result (map html/text [title artist])
-        link-result (anchors->hrefs [title artist])]
-    (zipmap [:title :artist :track-link :artist-link]
-            (concat text-result link-result))))
+  [node]
+  (let [shaker (key-shake node)
+        sq {:title [:.title :.trackTitle]
+            :artist [:.title :.trackArtist :a]}]
+    (nsmap {:links (shaker anchors->hrefs sq)
+            :text  (shaker #(map html/text %) sq)})))
 
 (comment ".: Threading to the max
   Here we're composing a bunch of steps to get to a valid search link,
@@ -75,7 +234,7 @@ like this."
               :track-link
               (str *base-url*)) ; "https://whosampled.com/Nas/Halftime"
 
-         ;; regress e2e
+         ;; this is the whole program so far
          (->> ["halftime" "nas"]
               (build-search *search-url*)
               fetch-url
@@ -87,6 +246,38 @@ like this."
          "I'm still thinking of how to write tests as a part of this process;
 still lots of reading to do."
          ":threading:composition:")
+
+;; this is the whole program so far
+(->> ["halftime" "nas"]
+     (build-search *search-url*)
+     fetch-url
+     search->tophit
+     track->info
+     :links/title
+     (str *base-url*))
+
+;;; DETAIL
+;;
+
+(def ^:dynamic *detail-url*
+  "https://whosampled.com/Nas/Halftime")
+
+(def ^:dynamic *detail-page*
+  (fetch-url *detail-url*))
+
+(def pred-sampled-in)
+;; (defn section->tracks
+;;   ""
+;;   ([nodes]
+;;    (section->tracks nodes [pred-sampled-in]))
+;;   ([nodes preds]
+;;    (map track->info (html/select nodes
+;;                                  [(concat [] preds)])))
+
+
+(commment "Enlive: Building Predicates
+I'm attempting to express the following set of constraints
+get")
 
 
 
@@ -210,6 +401,10 @@ Side note: My go-to at this point is `->`, the top-down threading macro; it
 seems like a great way to structure chained function calls, but my concern is
 that each function is doing too much if a default is to reach for this macro.
  "
+
+         (defn node->text
+           [n]
+           (html/text (first n)))
 
          (-> *base-page*
              (html/select [:head :title])
